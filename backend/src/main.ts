3cf456eec632d76560e40d5e33f3a2476e56baf2
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-05-18 23:52:10 +0200

    feat: configure Swagger API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { logger } from './common/logger';
@@ -11,6 +12,16 @@ async function bootstrap() {
 
   // Security headers
   app.use(helmet());
+
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('0Sync API')
+    .setDescription('The 0Sync bi-directional synchronization platform API')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   // Global validation pipe
   app.useGlobalPipes(
